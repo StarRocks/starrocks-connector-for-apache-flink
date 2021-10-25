de00e50ddee5dc187eaa30e96a81c2161f998e67
waittting <491771532@qq.com>
2021-10-26 02:30:18 +0800

    add table sql relatd class

--- src/main/java/com/starrocks/connector/flink/table/StarRocksDynamicTableSourceFactory.java
+++ src/main/java/com/starrocks/connector/flink/table/StarRocksDynamicTableSourceFactory.java
@@ -4,7 +4,9 @@ import org.apache.flink.configuration.ConfigOption;
 import org.apache.flink.configuration.ConfigOptions;
 import org.apache.flink.configuration.ReadableConfig;
 import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.connector.sink.DynamicTableSink;
 import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.factories.DynamicTableSinkFactory;
 import org.apache.flink.table.factories.DynamicTableSourceFactory;
 import org.apache.flink.table.factories.FactoryUtil;
 import org.apache.flink.table.utils.TableSchemaUtils;
@@ -12,63 +14,47 @@ import org.apache.flink.table.utils.TableSchemaUtils;
 import java.util.HashSet;
 import java.util.Set;
 
-public class StarRocksDynamicTableSourceFactory implements DynamicTableSourceFactory {
-
-    public static final ConfigOption<String> USERNAME = ConfigOptions
-            .key("username")
-            .stringType()
-            .noDefaultValue()
-            .withDescription("StarRocks username.");
-
-    public static final ConfigOption<String> PASSWORD = ConfigOptions
-            .key("password")
-            .stringType()
-            .noDefaultValue()
-            .withDescription("StarRocks password.");
-
-    public static final ConfigOption<String> FILTER = ConfigOptions
-            .key("filter")
-            .stringType()
-            .noDefaultValue()
-            .withDescription("filter");
+public final class StarRocksDynamicTableSourceFactory implements DynamicTableSourceFactory {
 
     @Override
     public DynamicTableSource createDynamicTableSource(Context context) {
 
-        final FactoryUtil.TableFactoryHelper factoryHelper = FactoryUtil.createTableFactoryHelper(this, context);
-        factoryHelper.validate();
+        final FactoryUtil.TableFactoryHelper helper = FactoryUtil.createTableFactoryHelper(this, context);
+        helper.validateExcept(StarRocksSourceOptions.SOURCE_PROPERTIES_PREFIX);
+        ReadableConfig options = helper.getOptions();
+        // validate some special properties
+        StarRocksSourceOptions sourceOptions = new StarRocksSourceOptions(options, context.getCatalogTable().getOptions());
         TableSchema physicalSchema = TableSchemaUtils.getPhysicalSchema(context.getCatalogTable().getSchema());
-        return new StarRocksDynamicTableSource(getStarRocksSourceOptions(factoryHelper.getOptions()));
+        return new StarRocksDynamicTableSource(sourceOptions, physicalSchema);
     }
 
-    private StarRocksSourceOptions getStarRocksSourceOptions(ReadableConfig config) {
-
-        StarRocksSourceOptions options = StarRocksSourceOptions.builder()
-                .withProperty("jdbc-url", config.get(null))
-                .build();
-        // todo: params
-        return options;
-    }
 
     @Override
     public String factoryIdentifier() {
-        return "starrocks";
+        return "starrocks-source";
     }
 
     @Override
     public Set<ConfigOption<?>> requiredOptions() {
+
         final Set<ConfigOption<?>> options = new HashSet<>();
-        options.add(USERNAME);
-        options.add(PASSWORD);
-        // todo: add params
+        options.add(StarRocksSourceOptions.USERNAME);
+        options.add(StarRocksSourceOptions.PASSWORD);
+        options.add(StarRocksSourceOptions.TABLE_NAME);
+        options.add(StarRocksSourceOptions.DATABASE_NAME);
+        options.add(StarRocksSourceOptions.HTTP_NODES);
         return options;
     }
 
     @Override
     public Set<ConfigOption<?>> optionalOptions() {
+
         final Set<ConfigOption<?>> options = new HashSet<>();
-        options.add(FILTER);
-        // todo: add params
+        options.add(StarRocksSourceOptions.BE_SOCKET_TIMEOUT);
+        options.add(StarRocksSourceOptions.BE_CONNECT_TIMEOUT);
+        options.add(StarRocksSourceOptions.BATCH_SIZE);
+        options.add(StarRocksSourceOptions.QUERTY_TIMEOUT);
+        options.add(StarRocksSourceOptions.MEM_LIMIT);
         return options;
     }
 }
--- src/test/java/com/starrocks/connector/flink/StarRocksSourceTest.java
+++ src/test/java/com/starrocks/connector/flink/StarRocksSourceTest.java
@@ -1,36 +1,34 @@
 package com.starrocks.connector.flink;
 
-
-import com.starrocks.connector.flink.manager.StarRocksSourceManager;
-import com.starrocks.connector.flink.related.QueryInfo;
-import com.starrocks.connector.flink.table.StarRocksSourceOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
 
 public class StarRocksSourceTest {
 
-
     public static void main(String[] args) throws Exception {
 
-        StarRocksSourceOptions options = StarRocksSourceOptions.builder()
-                .withProperty("jdbc-url", "jdbc:mysql://172.26.92.152:9632")
-                .withProperty("http-nodes", "172.26.92.152:8634,172.26.92.152:8634,172.26.92.152:8634")
-                .withProperty("be-socket-timeout", "5000")
-//                .withProperty("be-connect-timeout", "5000")
-                .withProperty("username", "root")
-                .withProperty("password", "")
-                .withProperty("table-name", "test_1")
-                .withProperty("database-name", "cjs_test")
-                .withProperty("columns", "col1, event_day")
-//                .withProperty("filter", "col1 = 0")
-                .build();
-
-        StarRocksSourceManager manager = new StarRocksSourceManager(options);
-        QueryInfo queryInfo = manager.getQueryInfo();
-
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(queryInfo.getBeXTablets().size());
-        env.addSource(new StarRocksSource(options, queryInfo)).print();
-        env.execute("StarRocks flink source");
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        final StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
+        tEnv.executeSql(
+                "CREATE TABLE test_1 (" +
+                        "col1 TINYINT," +
+                        "event_day STRING " +
+                        ") " +
+                        "WITH (\n" +
+                        "  'connector' = 'starrocks-source',\n" +
+                        "  'http-nodes' = '172.26.92.152:8634,172.26.92.152:8634,172.26.92.152:8634',\n" +
+                        "  'be-socket-timeout-ms' = '50000',\n" +
+                        "  'be-connect-timeout-ms' = '50000',\n" +
+                        "  'username' = 'root',\n" +
+                        "  'password' = '',\n" +
+                        "  'database-name' = 'cjs_test',\n" +
+                        "  'table-name' = 'test_1'\n" +
+                        ")");
+        final Table result = tEnv.sqlQuery("SELECT * from test_1");
+        tEnv.toRetractStream(result, Row.class).print();
+        env.execute();
     }
 }
