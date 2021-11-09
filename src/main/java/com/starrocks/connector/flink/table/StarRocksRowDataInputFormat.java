c019605029a6f58e43a4ceecc4b5995b8e237dae
waittting <491771532@qq.com>
2021-11-09 12:30:10 +0800

    bug fix: support isNull & boolean = true/false

--- src/main/java/com/starrocks/connector/flink/table/StarRocksRowDataInputFormat.java
+++ src/main/java/com/starrocks/connector/flink/table/StarRocksRowDataInputFormat.java
@@ -30,6 +30,7 @@ import java.util.ArrayList;
 
 
 import java.util.List;
+import java.util.Map;
 
 
 
@@ -85,6 +86,7 @@ public class StarRocksRowDataInputFormat extends RichInputFormat<RowData, StarRo
             });
             this.selectColumns = nColumns.toArray(new SelectColumn[0]);
         }
+        // validateTableStructure(this.colunmRichInfos);
     }
 
     @Override
@@ -213,6 +215,26 @@ public class StarRocksRowDataInputFormat extends RichInputFormat<RowData, StarRo
     @Override
     public TypeInformation<RowData> getProducedType() {
         return null;
+    }
+
+    private void validateTableStructure(List<ColunmRichInfo> colunmRichInfos) {
+        if (null == colunmRichInfos) {
+            return;
+        }
+        List<Map<String, Object>> rows = starrocksQueryVisitor.getTableColumnsMetaData();
+        if (null == rows) {
+            throw new IllegalArgumentException("Couldn't get the sink table's column info.");
+        }
+        // validate primary keys
+        if (colunmRichInfos.size() != rows.size()) {
+            throw new IllegalArgumentException("Fields count mismatch.");
+        }
+        for (int i = 0; i < rows.size(); i++) {
+            String starrocksField = rows.get(i).get("COLUMN_NAME").toString().toLowerCase();
+            String starrocksType = rows.get(i).get("DATA_TYPE").toString().toLowerCase();
+            // TODO:
+            // validateTableStructure
+        }
     }
 
     /** Builder for {@link StarRocksRowDataInputFormat}. */
