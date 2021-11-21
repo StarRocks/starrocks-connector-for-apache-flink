f123528595a3e9e5e6910c9b6a169efc880ebf19
waittting <491771532@qq.com>
2021-11-21 14:57:34 +0800

    Delete useless import

--- src/main/java/com/starrocks/connector/flink/table/source/StarRocksDynamicSourceFunction.java
+++ src/main/java/com/starrocks/connector/flink/table/source/StarRocksDynamicSourceFunction.java
@@ -16,7 +16,6 @@ import org.apache.flink.table.api.TableSchema;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
