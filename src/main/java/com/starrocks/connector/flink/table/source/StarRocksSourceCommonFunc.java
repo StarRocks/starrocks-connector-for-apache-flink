dce63fb8fa705da478643961fb45072a6d6f497a
waittting <491771532@qq.com>
2022-01-06 16:18:56 +0800

    Remove redundant else block

--- src/main/java/com/starrocks/connector/flink/table/source/StarRocksSourceCommonFunc.java
+++ src/main/java/com/starrocks/connector/flink/table/source/StarRocksSourceCommonFunc.java
@@ -71,63 +71,63 @@ public class StarRocksSourceCommonFunc {
                 curBeXTabletList.set(i, Arrays.asList(queryInfo.getBeXTablets().get(i)));
             }
             return curBeXTabletList;
-        } else if (subTaskCount < beXTabletsListCount) {
+        } 
+        if (subTaskCount < beXTabletsListCount) {
             for (int i = 0; i < beXTabletsListCount; i ++) {
                 List<QueryBeXTablets> tList = curBeXTabletList.get(i%subTaskCount);
                 tList.add(queryInfo.getBeXTablets().get(i));
                 curBeXTabletList.set(i%subTaskCount, tList);
             }
             return curBeXTabletList;
-        } else {
-            List<QueryBeXTablets> beWithSingleTabletList = new ArrayList<>();
-            queryInfo.getBeXTablets().forEach(beXTablets -> {
-                beXTablets.getTabletIds().forEach(tabletId -> {
-                    QueryBeXTablets beXOnlyOneTablets = new QueryBeXTablets(beXTablets.getBeNode(), Arrays.asList(tabletId));
-                    beWithSingleTabletList.add(beXOnlyOneTablets);
-                });
+        } 
+        List<QueryBeXTablets> beWithSingleTabletList = new ArrayList<>();
+        queryInfo.getBeXTablets().forEach(beXTablets -> {
+            beXTablets.getTabletIds().forEach(tabletId -> {
+                QueryBeXTablets beXOnlyOneTablets = new QueryBeXTablets(beXTablets.getBeNode(), Arrays.asList(tabletId));
+                beWithSingleTabletList.add(beXOnlyOneTablets);
             });
-            double x = (double)beWithSingleTabletList.size()/subTaskCount;
-            if (x <= 1) {
-                for (int i = 0; i < beWithSingleTabletList.size(); i ++) {
-                    curBeXTabletList.set(i, Arrays.asList(beWithSingleTabletList.get(i)));
-                }
-                return curBeXTabletList;
-            } 
-            long newx = Math.round(x);
-            for (int i = 0; i < subTaskCount; i ++) {
-                int start = (int)(i * newx);
-                int end = start + (int)newx;
-                List<QueryBeXTablets> curBxTs = new ArrayList<>();
-                if (start >= beWithSingleTabletList.size()) {
-                    continue;
-                }
-                if (end >= beWithSingleTabletList.size()) {
-                    end = beWithSingleTabletList.size();
-                }
-                if (i == subTaskCount - 1) {
-                    end = beWithSingleTabletList.size();
-                }
-                curBxTs = beWithSingleTabletList.subList(start, end);
-                Map<String, List<Long>> beXTabletsMap = new HashMap<>();
-                curBxTs.forEach(curBxT -> {
-                    List<Long> tablets = new ArrayList<>(); 
-                    if (beXTabletsMap.containsKey(curBxT.getBeNode())) {
-                        tablets = beXTabletsMap.get(curBxT.getBeNode());
-                    } else {
-                        tablets = new ArrayList<>();
-                    }
-                    tablets.add(curBxT.getTabletIds().get(0));
-                    beXTabletsMap.put(curBxT.getBeNode(), tablets);
-                });
-                List<QueryBeXTablets> tList = new ArrayList<>();
-                beXTabletsMap.forEach((beNode, tabletIds) -> {
-                    QueryBeXTablets queryBeXTablets = new QueryBeXTablets(beNode, tabletIds);
-                    tList.add(queryBeXTablets);
-                });
-                curBeXTabletList.set(i, tList);
+        });
+        double x = (double)beWithSingleTabletList.size()/subTaskCount;
+        if (x <= 1) {
+            for (int i = 0; i < beWithSingleTabletList.size(); i ++) {
+                curBeXTabletList.set(i, Arrays.asList(beWithSingleTabletList.get(i)));
             }
             return curBeXTabletList;
+        } 
+        long newx = Math.round(x);
+        for (int i = 0; i < subTaskCount; i ++) {
+            int start = (int)(i * newx);
+            int end = start + (int)newx;
+            List<QueryBeXTablets> curBxTs = new ArrayList<>();
+            if (start >= beWithSingleTabletList.size()) {
+                continue;
+            }
+            if (end >= beWithSingleTabletList.size()) {
+                end = beWithSingleTabletList.size();
+            }
+            if (i == subTaskCount - 1) {
+                end = beWithSingleTabletList.size();
+            }
+            curBxTs = beWithSingleTabletList.subList(start, end);
+            Map<String, List<Long>> beXTabletsMap = new HashMap<>();
+            curBxTs.forEach(curBxT -> {
+                List<Long> tablets = new ArrayList<>(); 
+                if (beXTabletsMap.containsKey(curBxT.getBeNode())) {
+                    tablets = beXTabletsMap.get(curBxT.getBeNode());
+                } else {
+                    tablets = new ArrayList<>();
+                }
+                tablets.add(curBxT.getTabletIds().get(0));
+                beXTabletsMap.put(curBxT.getBeNode(), tablets);
+            });
+            List<QueryBeXTablets> tList = new ArrayList<>();
+            beXTabletsMap.forEach((beNode, tabletIds) -> {
+                QueryBeXTablets queryBeXTablets = new QueryBeXTablets(beNode, tabletIds);
+                tList.add(queryBeXTablets);
+            });
+            curBeXTabletList.set(i, tList);
         }
+        return curBeXTabletList;
     }
 
     // public static void validateTableStructure(StarRocksSourceOptions sourceOptions, TableSchema flinkSchema) {
