388bffbddea8081a12528fa2531ec85544bba1c6
waittting <491771532@qq.com>
2022-03-09 21:50:05 +0800

    Add source code & add project starrocks-thrift-sdk

---
+++ src/main/java/com/starrocks/connector/flink/table/source/StarRocksSourceTrickReader.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.starrocks.connector.flink.table.source;
+
+import java.io.Serializable;
+
+
+import org.apache.flink.table.data.GenericRowData;
+
+public class StarRocksSourceTrickReader implements StarRocksSourceDataReader, Serializable {
+    private Long dataCount;
+    public StarRocksSourceTrickReader(Long dataCount) {
+        this.dataCount = dataCount;
+    }
+
+    @Override
+    public GenericRowData getNext() {
+        if (dataCount > 0) {
+            dataCount--;
+            return new GenericRowData(0);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return dataCount > 0;
+    }
+
+    @Override
+    public void close() { }
+}
