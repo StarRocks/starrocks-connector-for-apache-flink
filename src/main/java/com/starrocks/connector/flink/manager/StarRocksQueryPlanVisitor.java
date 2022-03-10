0a52f934bc35216141f670629c2d4631e6f74c5e
waittting <491771532@qq.com>
2022-03-10 15:19:10 +0800

    Add source code & add project starrocks-thrift-sdk (#55)

---
+++ src/main/java/com/starrocks/connector/flink/manager/StarRocksQueryPlanVisitor.java
@@ -0,0 +1,149 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.starrocks.connector.flink.manager;
+
+import com.alibaba.fastjson.JSONObject;
+import com.starrocks.connector.flink.table.source.StarRocksSourceOptions;
+import com.starrocks.connector.flink.table.source.struct.QueryBeXTablets;
+import com.starrocks.connector.flink.table.source.struct.QueryInfo;
+import com.starrocks.connector.flink.table.source.struct.QueryPlan;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+
+
+public class StarRocksQueryPlanVisitor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOG = LoggerFactory.getLogger(StarRocksQueryPlanVisitor.class);
+    private StarRocksSourceOptions sourceOptions;
+
+    public StarRocksQueryPlanVisitor(StarRocksSourceOptions sourceOptions) {
+        this.sourceOptions = sourceOptions;
+    }
+
+    public void setSourceOptions(StarRocksSourceOptions sourceOptions) {
+        this.sourceOptions = sourceOptions;
+    }
+
+    public QueryInfo getQueryInfo(String SQL) throws IOException {
+        LOG.info("query sql [{}]", SQL);
+        String[] httpNodes = sourceOptions.getScanUrl().split(",");
+        QueryPlan plan = getQueryPlan(SQL, httpNodes[new Random().nextInt(httpNodes.length)], sourceOptions);
+        Map<String, Set<Long>> beXTablets = transferQueryPlanToBeXTablet(plan);
+        List<QueryBeXTablets> queryBeXTabletsList = new ArrayList<>();
+        beXTablets.entrySet().stream().forEach(entry -> {
+            QueryBeXTablets queryBeXTablets = new QueryBeXTablets(entry.getKey(), new ArrayList<>(entry.getValue()));
+            queryBeXTabletsList.add(queryBeXTablets);
+        });
+        return new QueryInfo(plan, queryBeXTabletsList);
+    }
+
+    private static Map<String, Set<Long>> transferQueryPlanToBeXTablet(QueryPlan queryPlan) {
+        Map<String, Set<Long>> beXTablets = new HashMap<>();
+        queryPlan.getPartitions().forEach((tabletId, routingList) -> {
+            int tabletCount = Integer.MAX_VALUE;
+            String candidateBe = "";
+            for (String beNode : routingList.getRoutings()) {
+                if (!beXTablets.containsKey(beNode)) {
+                    beXTablets.put(beNode, new HashSet<>());
+                    candidateBe = beNode;
+                    break;
+                } 
+                if (beXTablets.get(beNode).size() < tabletCount) {
+                    candidateBe = beNode;
+                    tabletCount = beXTablets.get(beNode).size();
+                }
+            }
+            beXTablets.get(candidateBe).add(Long.valueOf(tabletId));
+        });
+        return beXTablets;
+    }
+
+    private static QueryPlan getQueryPlan(String querySQL, String httpNode, StarRocksSourceOptions sourceOptions) throws IOException {
+        String url = new StringBuilder("http://")
+            .append(httpNode)
+            .append("/api/")
+            .append(sourceOptions.getDatabaseName())
+            .append("/")
+            .append(sourceOptions.getTableName())
+            .append("/_query_plan")
+            .toString();
+
+        Map<String, Object> bodyMap = new HashMap<>();
+        bodyMap.put("sql", querySQL);
+        String body = new JSONObject(bodyMap).toString();
+        int requsetCode = 0;
+        String respString = "";
+        for (int i = 0; i < sourceOptions.getScanMaxRetries(); i ++) {
+            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+                HttpPost post = new HttpPost(url);
+                post.setHeader("Content-Type", "application/json;charset=UTF-8");
+                post.setHeader("Authorization", getBasicAuthHeader(sourceOptions.getUsername(), sourceOptions.getPassword()));
+                post.setEntity(new ByteArrayEntity(body.getBytes()));
+                try (CloseableHttpResponse response = httpClient.execute(post)) {
+                    requsetCode = response.getStatusLine().getStatusCode();
+                    HttpEntity respEntity = response.getEntity();
+                    respString = EntityUtils.toString(respEntity, "UTF-8");
+                }
+            }
+            if (200 == requsetCode || i == sourceOptions.getScanMaxRetries() - 1) {
+                break;
+            }
+            LOG.warn("Request of get query plan failed with code:{}", requsetCode);
+            try {
+                Thread.sleep(1000l * (i + 1));
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+                throw new IOException("Unable to get query plan, interrupted while doing another attempt", ex);
+            }
+        }
+        if (200 != requsetCode) {
+            throw new RuntimeException("Request of get query plan failed with code " + requsetCode + " " + respString);
+        }
+        if (respString.isEmpty() || respString.equals("")) {
+            LOG.warn("Request failed with empty response.");
+            throw new RuntimeException("Request failed with empty response." + requsetCode);
+        }
+        JSONObject jsonObject = JSONObject.parseObject(respString);
+        return JSONObject.toJavaObject(jsonObject, QueryPlan.class);
+    }
+
+    private static String getBasicAuthHeader(String username, String password) {
+        String auth = username + ":" + password;
+        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(StandardCharsets.UTF_8));
+        return new StringBuilder("Basic ").append(new String(encodedAuth)).toString();
+    }
+}
---
+++ src/test/java/com/starrocks/connector/flink/it/source/StarRocksDynamicTableSourceITTest.java
@@ -0,0 +1,159 @@
+package com.starrocks.connector.flink.it.source;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import com.starrocks.connector.flink.StarRocksSource;
+import com.starrocks.connector.flink.table.source.StarRocksSourceCommonFunc;
+import com.starrocks.connector.flink.table.source.StarRocksSourceOptions;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.data.RowData;
+import org.junit.Test;
+
+import mockit.Mock;
+import mockit.MockUp;
+
+
+public class StarRocksDynamicTableSourceITTest extends StarRocksSourceBaseTest {
+
+    private Long dataCount = 30L;
+
+    @Test
+    public void testDataStream() throws Exception {
+        new MockUp<StarRocksSourceCommonFunc>() {
+            @Mock
+            public Long getQueryCount(StarRocksSourceOptions sourceOptions, String SQL) {
+                return dataCount;
+            }
+        };
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        List<RowData> dList = env.addSource(StarRocksSource.source(TABLE_SCHEMA, OPTIONS_WITH_COLUMN_IS_COUNT)).setParallelism(5).executeAndCollect(50);
+        assertTrue(dList.size() == dataCount);
+    }
+
+    @Test
+    public void testTableAPI() {
+        mockOneBeResonsefunc();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        StreamTableEnvironment tEnv;
+        tEnv = StreamTableEnvironment.create(env);
+        tEnv.executeSql(
+                "CREATE TABLE flink_type_test (" +
+                    "date_1 DATE," +
+                    "datetime_1 TIMESTAMP(6),"+
+                    "char_1 CHAR(20),"+
+                    "varchar_1 VARCHAR,"+
+                    "boolean_1 BOOLEAN,"+
+                    "tinyint_1 TINYINT,"+
+                    "smallint_1 SMALLINT,"+
+                    "int_1 INT,"+
+                    "bigint_1 BIGINT,"+
+                    "largeint_1 STRING,"+
+                    "float_1 FLOAT,"+
+                    "double_1 DOUBLE,"+
+                    "decimal_1 DECIMAL(10,9)\n"+
+                ") WITH (\n" +
+                    "  'connector' = 'starrocks',\n" +
+                    "  'scan-url' = '" + OPTIONS.getScanUrl() + "',\n" +
+                    "  'scan.connect.timeout-ms' = '5000', " +
+                    "  'jdbc-url' = '" + OPTIONS.getJdbcUrl() + "',\n" +
+                    "  'username' = '" + OPTIONS.getUsername() + "',\n" +
+                    "  'password' = '" + OPTIONS.getPassword() + "',\n" +
+                    "  'database-name' = '" + OPTIONS.getDatabaseName() + "',\n" +
+                    "  'table-name' = '" + OPTIONS.getTableName() + "')"
+                );
+        Exception e = null;
+        try {
+            tEnv.executeSql("select * from flink_type_test").print();;
+            Thread.sleep(5000);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            e = ex;
+        }
+        assertTrue(e == null);
+    }
+
+    @Test
+    public void testLookupTableAPI() {
+        Exception e = null;
+        try {            
+            Thread thriftThread = new Thread(() -> {
+                mockOneBeResonsefunc();
+                StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+                env.setParallelism(1);
+                StreamTableEnvironment tEnv;
+                tEnv = StreamTableEnvironment.create(env);
+                tEnv.executeSql(
+                    "CREATE TABLE datagen (" +
+                        "k1 BOOLEAN,"+
+                        "k2 SMALLINT,"+
+                        "proctime as PROCTIME() " +
+                    ") WITH ( " +
+                        "'connector' = 'datagen', " +
+                        "'rows-per-second'='1'" +
+                    ")"
+                );
+                tEnv.executeSql(
+                    "CREATE TABLE flink_type_test (" +
+                        "date_1 DATE," +
+                        "datetime_1 TIMESTAMP(6),"+
+                        "char_1 CHAR(20),"+
+                        "varchar_1 VARCHAR,"+
+                        "boolean_1 BOOLEAN,"+
+                        "tinyint_1 TINYINT,"+
+                        "smallint_1 SMALLINT,"+
+                        "int_1 INT,"+
+                        "bigint_1 BIGINT,"+
+                        "largeint_1 STRING,"+
+                        "float_1 FLOAT,"+
+                        "double_1 DOUBLE,"+
+                        "decimal_1 DECIMAL(10,9)\n"+
+                    ") WITH (\n" +
+                        "'connector' = 'starrocks',\n" +
+                        "'scan-url' = '" + OPTIONS.getScanUrl() + "',\n" +
+                        "'scan.connect.timeout-ms' = '5000', " +
+                        "'jdbc-url' = '" + OPTIONS.getJdbcUrl() + "',\n" +
+                        "'username' = '" + OPTIONS.getUsername() + "',\n" +
+                        "'password' = '" + OPTIONS.getPassword() + "',\n" +
+                        "'database-name' = '" + OPTIONS.getDatabaseName() + "',\n" +
+                        "'table-name' = '" + OPTIONS.getTableName() + "')"
+                );
+                tEnv.executeSql("SELECT * FROM datagen LEFT JOIN flink_type_test FOR SYSTEM_TIME AS OF datagen.proctime ON " + 
+                                "datagen.k1 = flink_type_test.boolean_1").print();
+            });
+            thriftThread.start();
+            Thread.sleep(5000);
+            throw new RuntimeException("Time is up");
+        } catch (Exception ex) {
+            e = ex;
+        }
+        assertTrue(e.getMessage().equals("Time is up"));
+    }
+
+    @Test
+    public void testSourceCommonProperties() {
+        assertEquals(JDBC_URL, OPTIONS.getJdbcUrl());
+        assertEquals(SCAN_URL, OPTIONS.getScanUrl());
+        assertEquals(DATABASE, OPTIONS.getDatabaseName());
+        assertEquals(TABLE, OPTIONS.getTableName());
+        assertEquals(USERNAME, OPTIONS.getUsername());
+        assertEquals(PASSWORD, OPTIONS.getPassword());
+    }
+
+    private boolean checkCause(Throwable throwable, String causeStr) {
+        if (null == throwable) {
+            return false;
+        }
+        if (throwable.getMessage().contains(causeStr)) {
+            return true;
+        }
+        return checkCause(throwable.getCause(), causeStr);
+    }
+}
